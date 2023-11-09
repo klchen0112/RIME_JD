7deba7649018fcd9536bbd6e3ef43c90658a48ca
klchen0112 <klchen0112@gmail.com>
2023-11-09 14:51:29 +0800

    update rime-ice

--- lua/is_in_user_dict.lua
+++ lua/is_in_user_dict.lua
@@ -1,11 +1,28 @@
--- 为用户词典中（输入过）的内容结尾加上一个星号 *
-local function is_in_user_dict(input, env)
+-- 根据是否在用户词典，在结尾加上一个星号 *
+-- is_in_user_dict: true           输入过的内容
+-- is_in_user_dict: flase 或不写    未输入过的内容
+
+local M = {}
+
+function M.init(env)
+    local config = env.engine.schema.config
+    env.name_space = env.name_space:gsub('^*', '')
+    M.is_in_user_dict = config:get_bool(env.name_space)
+end
+
+function M.func(input, env)
     for cand in input:iter() do
-        if (string.find(cand.type, "user")) then
-            cand.comment = cand.comment .. '*'
+        if M.is_in_user_dict then
+            if cand.type == "user_phrase" then
+                cand.comment = cand.comment .. '*'
+            end
+        else
+            if cand.type ~= "user_phrase" then
+                cand.comment = cand.comment .. '*'
+            end
         end
         yield(cand)
     end
 end
 
-return is_in_user_dict
+return M
